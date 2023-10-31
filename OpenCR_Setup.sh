23289bc42e497dc6743843f83b8162b7b253df30
DoveSensei <133796894+DoveSensei@users.noreply.github.com>
2023-11-01 07:17:07 +0900

    Add files via upload

--- OpenCR_Setup.sh
+++ OpenCR_Setup.sh
@@ -1,9 +1,28 @@
+#!/bin/bash
+
+# 초기 설정 모드(인자 1)의 경우 종속성 라이브러리 설치 진행
+if [ "$1" = "1" ]; then
+    echo "\033[33msudo dpkg --add-architecture armhf\033[0m"
+    sudo dpkg --add-architecture armhf
+    echo "\033[33msudo apt-get update\033[0m"
+    sudo apt-get update
+    echo "\033[33msudo apt-get install -y libc6:armhf\033[0m"
+    sudo apt-get install -y libc6:armhf
+fi
+
+
 export OPENCR_PORT=/dev/ttyACM0
 export OPENCR_MODEL=burger_noetic
 
 cd $HOME
-rm -rf ./opencr_update.tar.bz2
-wget https://github.com/ROBOTIS-GIT/OpenCR-Binaries/raw/master/turtlebot3/ROS1/latest/opencr_update.tar.bz2 
-tar -xvf opencr_update.tar.bz2 
+
+if [ -f "opencr_update.tar.bz2" ]; then
+    rm -rf opencr_update
+    echo "\033[33mtar zxvf ./opencr_update.tar.bz2\033[0m"
+    tar -xvf opencr_update.tar.bz2
+else
+    wget https://github.com/ROBOTIS-GIT/OpenCR-Binaries/raw/master/turtlebot3/ROS1/latest/opencr_update.tar.bz2
+fi
+
 cd ./opencr_update
 ./update.sh $OPENCR_PORT $OPENCR_MODEL.opencr
--- turtlebotRobot_setup.sh
+++ turtlebotRobot_setup.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+
+if [ "$1" = "1" ]; then
+    echo "\033[33msudo apt-get install ros-noetic-rosserial-python\033[0m"
+    sudo apt-get install -y ros-noetic-rosserial-python
+fi
+
 cd $HOME
 
 echo "\033[33mInit ~/.bashrc\033[0m"
@@ -14,11 +20,28 @@ echo "\033[33mAdd export TURTLEBOT3_MODEL=burger\033[0m"
 echo "export TURTLEBOT3_MODEL=burger" >> ~/.bashrc
 
 echo "\033[33mAdd export LDS_MODEL=LDS-01\033[0m"
-echo "export export LDS_MODEL=LDS-01" >> ~/.bashrc
+echo "export export LDS_MODEL=LDS-02" >> ~/.bashrc
 
 echo "\033[33mrm -rf turtlebot\033[0m"
 rm -rf turtlebot
 
-echo "\033[33mtar zxvf turtlebot.tar.gz\033[0m"
-tar zxvf turtlebot.tar.gz
+#echo "\033[33mtar zxvf turtlebot.tar.gz\033[0m"
+#tar zxvf turtlebot.tar.gz
 
+if [ -f "turtlebot.tar.gz" ]; then
+    echo "\033[33mtar zxvf turtlebot.tar.gz\033[0m"
+    tar zxvf turtlebot.tar.gz
+else
+    echo "\033[33mCreating turtlebot directory and cloning repositories\033[0m"
+    mkdir turtlebot
+    cd turtlebot
+    mkdir src
+    cd src
+    git clone -b develop https://github.com/ROBOTIS-GIT/ld08_driver.git
+    git clone -b noetic-devel https://github.com/ROBOTIS-GIT/turtlebot3.git
+    git clone -b noetic-devel https://github.com/ROBOTIS-GIT/turtlebot3_msgs.git
+    cd ..
+    catkin_make -j1
+    cd ..
+    tar zcvf turtlebot.tar.gz turtlebot
+fi
