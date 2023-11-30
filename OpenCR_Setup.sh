cd997e096abf11e4d94e778309360d06a459f917
DoveSensei <133796894+DoveSensei@users.noreply.github.com>
2023-11-30 21:03:20 +0900

    Add files via upload

--- OpenCR_Setup.sh
+++ OpenCR_Setup.sh
@@ -1,18 +1,39 @@
 #!/bin/bash
 
+#[burger, waffle, waffle_pi]
+turtlebot_model="burger"
+ros_version=$(rosversion -d)
+
 # 초기 설정 모드(인자 1)의 경우 종속성 라이브러리 설치 진행
 if [ "$1" = "1" ]; then
-    echo "\033[33msudo dpkg --add-architecture armhf\033[0m"
-    sudo dpkg --add-architecture armhf
     echo "\033[33msudo apt-get update\033[0m"
     sudo apt-get update
+    echo "\033[33msudo apt-get upgrade\033[0m"
+    sudo apt-get upgrade
+    echo "\033[33msudo dpkg --add-architecture armhf\033[0m"
+    sudo dpkg --add-architecture armhf
     echo "\033[33msudo apt-get install -y libc6:armhf\033[0m"
     sudo apt-get install -y libc6:armhf
 fi
 
 
+#OPENCR_MODEL 이름 설정
+if [ "$turtlebot_model" = "burger" ]; then
+    opencr_name_1="burger"
+else
+    opencr_name_1="waffle"
+fi
+
+if [ "$ros_version" = "noetic" ]; then
+    opencr_name_2="_noetic"
+else
+    opencr_name_2=""
+fi
+
+echo "\033[33mexport OPENCR_PORT=/dev/ttyACM0\033[0m"
 export OPENCR_PORT=/dev/ttyACM0
-export OPENCR_MODEL=burger_noetic
+echo "\033[33mexport OPENCR_MODEL=${opencr_name_1}${opencr_name_2}\033[0m"
+export OPENCR_MODEL=${opencr_name_1}${opencr_name_2}
 
 cd $HOME
 
